503c40cd1cedef503c4a0f13fec673c5f2637889
nematatu <nematatu1003@gmail.com>
2025-05-29 21:06:54 +0900

    fix: metadata編集

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { fredoka } from "@/utils/fonts/v1/fonts";
 
 export const metadata: Metadata = {
+    metadataBase: new URL("https://amatatu.com"),
     openGraph: {
         title: "amatatu",
         description: "amatatu's portfolio",
