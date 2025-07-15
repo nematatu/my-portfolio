8c55b99cd2d045d152d55f6be268b92536e8c6b7
nematatu <nematatu1003@gmail.com>
2025-07-15 22:58:50 +0900

    style: :lipstick: 画像サイズのレスポンシブ対応
    スマホでプレビューできて嬉しい

--- src/layouts/v2/Home.tsx
+++ src/layouts/v2/Home.tsx
@@ -3,7 +3,7 @@ import Logo from "@/components/icons/logo.svg";
 export default function Home() {
 	return (
 		<div className="bg-gray-100 min-h-screen min-w-screen flex items-center justify-center">
-			<div className=" w-full md:w-3/1 sm:w-1/2 lg:w-1/4 p-8">
+			<div className="w-80 md:w-1/3 lg:w-1/4 p-8">
 				<div className="rounded-[28px] bg-white p-[8px]">
 					<Logo className="rounded-[20px]" />
 				</div>
