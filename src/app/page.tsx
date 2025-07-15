1255c9e17633f0811c9e553b89ad71386db9ee65
nematatu <nematatu1003@gmail.com>
2025-07-15 18:33:56 +0900

    feat: :sparkles: v1のコンポーネントをv2に入れ替え

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,15 @@
 
 import Background from "@/components/backgrounds/v1-bg.svg";
 import Header from "@/components/layouts/v1/Header";
-import Home from "@/components/layouts/v1/Home";
-import Profile from "@/components/layouts/v1/Profile";
-import Projects from "@/components/layouts/v1/Projects";
-import ParticlesBackground from "@/components/layouts/v1/particles";
+import Home from "@/components/layouts/v2/Home";
 
 export default function MinimalPortfolio() {
 	return (
 		<div className=" min-h-screen  text-white overflow-hidden relative">
 			<Background className="fixed inset-0 z-0" />
-			<ParticlesBackground />
 			<Header />
 			<main className="px-8 md:px-16 lg:px-24">
 				<Home />
-				<Profile />
-				<Projects />
 			</main>
 		</div>
 	);
