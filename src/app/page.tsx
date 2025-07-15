4a7ce99eaf12cd8ad8a52cbd3975a33dbec079cd
nematatu <nematatu1003@gmail.com>
2025-07-15 19:43:44 +0900

    feat: :sparkles: v1のHeaderとBackground消し去った
    真っ白だよ

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,10 @@
 "use client";
 
-import Background from "@/components/backgrounds/v1-bg.svg";
-import Header from "@/components/layouts/v1/Header";
 import Home from "@/components/layouts/v2/Home";
 
 export default function MinimalPortfolio() {
 	return (
 		<div className=" min-h-screen  text-white overflow-hidden relative">
-			<Background className="fixed inset-0 z-0" />
-			<Header />
 			<main className="px-8 md:px-16 lg:px-24">
 				<Home />
 			</main>
