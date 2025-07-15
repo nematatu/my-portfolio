2fa8aac53d2814e38be94bce4723520356eb0919
nematatu <nematatu1003@gmail.com>
2025-07-15 23:28:31 +0900

    style: :lipstick: 縦並びで横中央に配置

--- src/layouts/v2/Home.tsx
+++ src/layouts/v2/Home.tsx
@@ -2,14 +2,13 @@ import Logo from "@/components/icons/logo.svg";
 
 export default function Home() {
 	return (
-		<div className="bg-gray-100 min-h-screen min-w-screen flex items-center justify-start">
-				<div className="w-80 md:w-1/3 lg:w-1/4 p-12">
-					<div className="rounded-[28px] bg-white p-[8px]">
-						<Logo className="rounded-[20px]" />
-					</div>
+		<div className="bg-gray-100 min-h-screen min-w-screen flex flex-col items-center justify-start p-8">
+			<div className="w-80 md:w-1/3 lg:w-1/4 p-12">
+				<div className="rounded-[28px] bg-white p-[8px]">
+					<Logo className="rounded-[20px]" />
 				</div>
-				<p>hello, </p>
-				<p>world</p>
+			</div>
+			<p className="pt-12">Hello</p>
 		</div>
 	);
 }
