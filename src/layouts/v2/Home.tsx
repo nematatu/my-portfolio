7aba314af7dce1acb94b1238a8f3716a3faea8b4
nematatu <nematatu1003@gmail.com>
2025-07-15 21:48:50 +0900

    feat(Home): :sparkles: ロゴ表示、レスポンシブ対応

--- src/layouts/v2/Home.tsx
+++ src/layouts/v2/Home.tsx
@@ -1,7 +1,10 @@
+import Logo from "@/components/icons/logo.svg";
+
 export default function Home() {
 	return (
 		<div className="bg-gray-100 min-h-screen min-w-screen flex items-center justify-center">
-			<div>
+			<div className="bg-yellow-500 w-full md:w-3/1 sm:w-1/2 lg:w-1/4 p-8">
+				<Logo />
 				<p>hello</p>
 				<p>world</p>
 			</div>
