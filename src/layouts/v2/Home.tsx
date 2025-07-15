fe4a08cf47e4dfbc49be83b74993b2fd278a6bed
nematatu <nematatu1003@gmail.com>
2025-07-15 23:05:12 +0900

    style: :lipstick: paddingを大きくしてみた

--- src/layouts/v2/Home.tsx
+++ src/layouts/v2/Home.tsx
@@ -3,7 +3,7 @@ import Logo from "@/components/icons/logo.svg";
 export default function Home() {
 	return (
 		<div className="bg-gray-100 min-h-screen min-w-screen flex items-start justify-center">
-			<div className="w-80 md:w-1/3 lg:w-1/4 p-8">
+			<div className="w-80 md:w-1/3 lg:w-1/4 p-12">
 				<div className="rounded-[28px] bg-white p-[8px]">
 					<Logo className="rounded-[20px]" />
 				</div>
