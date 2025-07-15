34a27d64ff53e7fcc5675a8ef1558d86e6107356
nematatu <nematatu1003@gmail.com>
2025-07-15 22:40:21 +0900

    style: :lipstick: ロゴのroundedをappleのやつにした

--- src/layouts/v2/Home.tsx
+++ src/layouts/v2/Home.tsx
@@ -3,8 +3,10 @@ import Logo from "@/components/icons/logo.svg";
 export default function Home() {
 	return (
 		<div className="bg-gray-100 min-h-screen min-w-screen flex items-center justify-center">
-			<div className="bg-yellow-500 w-full md:w-3/1 sm:w-1/2 lg:w-1/4 p-8">
-				<Logo />
+			<div className=" w-full md:w-3/1 sm:w-1/2 lg:w-1/4 p-8">
+				<div className="rounded-[28px] bg-white p-[8px]">
+					<Logo className="rounded-[20px]" />
+				</div>
 				<p>hello</p>
 				<p>world</p>
 			</div>
