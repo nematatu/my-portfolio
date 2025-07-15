1263760cf57c6d1a081578a51c294df3abf43402
nematatu <nematatu1003@gmail.com>
2025-07-15 21:00:10 +0900

    style(Home): :lipstick: x軸真ん中に配置

--- src/layouts/v2/Home.tsx
+++ src/layouts/v2/Home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
     return (
         <div className="bg-gray-100 min-h-screen min-w-screen">
-            <div>
+            <div className="flex justify-center">
                 hello
             </div>
         </div>
