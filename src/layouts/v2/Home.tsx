b3c212adc1da964f6048c69decd86a036397634f
nematatu <nematatu1003@gmail.com>
2025-07-15 21:11:43 +0900

    style: :lipstick: 縦横中央に配置

--- src/layouts/v2/Home.tsx
+++ src/layouts/v2/Home.tsx
@@ -1,9 +1,10 @@
 export default function Home() {
-    return (
-        <div className="bg-gray-100 min-h-screen min-w-screen">
-            <div className="flex justify-center">
-                hello
-            </div>
-        </div>
-    )
+	return (
+		<div className="bg-gray-100 min-h-screen min-w-screen flex items-center justify-center">
+			<div>
+				<p>hello</p>
+				<p>world</p>
+			</div>
+		</div>
+	);
 }
