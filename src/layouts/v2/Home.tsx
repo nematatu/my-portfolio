8dbef002e3745943ae4cae12ec8116ad909f5850
nematatu <nematatu1003@gmail.com>
2025-07-15 23:21:34 +0900

    chore(思いついたデザイン案): :hammer: シンプルかと思ったらぶわぁぁぁ！！ってなる左端に寄せ過ぎなデザイン
    思いついた

--- src/layouts/v2/Home.tsx
+++ src/layouts/v2/Home.tsx
@@ -2,14 +2,14 @@ import Logo from "@/components/icons/logo.svg";
 
 export default function Home() {
 	return (
-		<div className="bg-gray-100 min-h-screen min-w-screen flex items-start justify-center">
-			<div className="w-80 md:w-1/3 lg:w-1/4 p-12">
-				<div className="rounded-[28px] bg-white p-[8px]">
-					<Logo className="rounded-[20px]" />
+		<div className="bg-gray-100 min-h-screen min-w-screen flex items-center justify-start">
+				<div className="w-80 md:w-1/3 lg:w-1/4 p-12">
+					<div className="rounded-[28px] bg-white p-[8px]">
+						<Logo className="rounded-[20px]" />
+					</div>
 				</div>
-				<p>hello</p>
+				<p>hello, </p>
 				<p>world</p>
-			</div>
 		</div>
 	);
 }
