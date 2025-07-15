e7384a61c8b638e7b60a26dbec91432ded6b46e5
nematatu <nematatu1003@gmail.com>
2025-07-15 18:29:41 +0900

    refactor: :recycle: v1のParticlesをコンポーネントに切り分けた

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,19 +5,13 @@ import Header from "@/components/layouts/v1/Header";
 import Home from "@/components/layouts/v1/Home";
 import Profile from "@/components/layouts/v1/Profile";
 import Projects from "@/components/layouts/v1/Projects";
-import Particles from "@/components/ui/particles";
+import ParticlesBackground from "@/components/layouts/v1/particles";
 
 export default function MinimalPortfolio() {
-	const color = "#ffffff";
 	return (
 		<div className=" min-h-screen  text-white overflow-hidden relative">
 			<Background className="fixed inset-0 z-0" />
-			<Particles
-				className="fixed inset-0 z-10"
-				quantity={30}
-				ease={100}
-				color={color}
-			/>
+			<ParticlesBackground />
 			<Header />
 			<main className="px-8 md:px-16 lg:px-24">
 				<Home />
