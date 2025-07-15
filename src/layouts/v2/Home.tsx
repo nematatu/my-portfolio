24307a5656ddf1b07c9e2e9d22d4848b80ac26ec
nematatu <nematatu1003@gmail.com>
2025-07-16 01:01:27 +0900

    style: :lipstick: 全体的に真ん中にしつつ、文字はその中で左寄せ

--- src/layouts/v2/Home.tsx
+++ src/layouts/v2/Home.tsx
@@ -2,13 +2,23 @@ import Logo from "@/components/icons/logo.svg";
 
 export default function Home() {
 	return (
-		<div className="bg-gray-100 min-h-screen min-w-screen flex flex-col items-center justify-start p-8">
-			<div className="w-80 md:w-1/3 lg:w-1/4 p-12">
-				<div className="rounded-[28px] bg-white p-[8px]">
-					<Logo className="rounded-[20px]" />
+		<div className="bg-gray-100 min-h-screen min-w-screen p-8 bg-blue-200">
+			<div className="w-full md: w-1/2 lg:w-1/3 mx-auto">
+				<div className="w-full p-12">
+					<div className="rounded-[28px] bg-white p-[8px]">
+						<Logo className="rounded-[20px]" />
+					</div>
+				</div>
+
+				<p className="text-4xl my-2 flex items-start justify-start">Amtt</p>
+				<div className="flex flex-col items-start justify-start">
+					<div className="space-y-9">
+						<p className="">About</p>
+						<p className="">Works</p>
+						<p className="">Blog</p>
+					</div>
 				</div>
 			</div>
-			<p className="pt-12">Hello</p>
 		</div>
 	);
 }
