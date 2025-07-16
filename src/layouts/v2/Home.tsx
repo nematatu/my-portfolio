1e17408d978bc747ed4a26657cb6dbec1b32adcf
nematatu <nematatu1003@gmail.com>
2025-07-16 16:49:47 +0900

    feat: :sparkles: アイコンを追加
    たい焼きが良いね
    レイアウトがまだ気に入ってない

--- src/layouts/v2/Home.tsx
+++ src/layouts/v2/Home.tsx
@@ -1,24 +1,39 @@
 import Logo from "@/components/icons/logo.svg";
 
-export default function Home() {
-	return (
-		<div className="bg-gray-100 min-h-screen min-w-screen p-8 bg-blue-200">
-			<div className="w-full md: w-1/2 lg:w-1/3 mx-auto">
-				<div className="w-full p-12">
-					<div className="rounded-[28px] bg-white p-[8px]">
-						<Logo className="rounded-[20px]" />
-					</div>
-				</div>
+import NewsPaper from "@/components/icons/japan/B_newspaper.svg";
+import Sushi from "@/components/icons/japan/F_sushi.svg";
+import Taiyaki from "@/components/icons/japan/F_taiyaki.svg";
 
-				<p className="text-4xl my-2 flex items-start justify-start">Amtt</p>
-				<div className="flex flex-col items-start justify-start">
-					<div className="space-y-9">
-						<p className="">About</p>
-						<p className="">Works</p>
-						<p className="">Blog</p>
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+export default function Home() {
+    return (
+        <div className="bg-gray-100 min-h-screen min-w-screen p-8">
+            <div className="w-3/4 lg:w-1/4 mx-auto">
+                <div className="w-full pt-12 pr-12 pl-12">
+                    <div className="rounded-[28px] bg-white p-[8px]">
+                        <Logo className="rounded-[20px]" />
+                    </div>
+                </div>
+
+                <div className="px-7">
+                    <p className="text-[30px] tracking-widest my-6">Amtt</p>
+                    <div>
+                        <div className="space-y-5 text-sm tracking-widest">
+                            <div className="flex space-x-5 items-center">
+                                <Taiyaki className="w-[24px]" />
+                                <p className="">About/</p>
+                            </div>
+                            <div className="flex space-x-5 items-center">
+                                <Sushi className="w-[24px]" />
+                                <p className="">Works/</p>
+                            </div>
+                            <div className="flex space-x-5 items-center">
+                                <NewsPaper className="w-[24px]" />
+                                <p className="">Blog/</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
