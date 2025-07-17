06eed58176a2a2e3f8fd49be9ca0792377998351
nematatu <nematatu1003@gmail.com>
2025-07-17 22:26:56 +0900

    fix: 🧑‍💻 前回同様、IBM_Plex_Monoを穢土転生

--- src/utils/fonts/v1/fonts.ts
+++ src/utils/fonts/v1/fonts.ts
@@ -1,4 +1,9 @@
-import { Fredoka, Zen_Kaku_Gothic_New } from "next/font/google";
+import { Fredoka, IBM_Plex_Mono, Zen_Kaku_Gothic_New } from "next/font/google";
+
+export const iBM_Plex_Mono = IBM_Plex_Mono({
+	subsets: ["latin"],
+	weight: ["400", "700"],
+});
 
 export const fredoka = Fredoka({
 	subsets: ["latin"],
@@ -6,6 +11,6 @@ export const fredoka = Fredoka({
 });
 
 export const zen_Kaku_Gothic_New = Zen_Kaku_Gothic_New({
-	subsets: ["latin"],
-	weight: ["400", "700"],
+    subsets: ["latin"],
+    weight: ["400", "700"],
 });
