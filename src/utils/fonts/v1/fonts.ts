cd981945735fd33b50982824f1857d2163ba2186
nematatu <nematatu1003@gmail.com>
2025-07-17 22:25:13 +0900

    fix: 🧑‍💻 v1ではfredoka使ってるのに消しちゃってたのが原因でエラー出てたので修正

--- src/utils/fonts/v1/fonts.ts
+++ src/utils/fonts/v1/fonts.ts
@@ -1,4 +1,10 @@
-import { Zen_Kaku_Gothic_New } from "next/font/google";
+import { Fredoka, Zen_Kaku_Gothic_New } from "next/font/google";
+
+export const fredoka = Fredoka({
+	subsets: ["latin"],
+	weight: ["400", "700"],
+});
+
 export const zen_Kaku_Gothic_New = Zen_Kaku_Gothic_New({
 	subsets: ["latin"],
 	weight: ["400", "700"],
