ab307d8703eea9c7b790959d235f35ab363f1d39
nematatu <nematatu1003@gmail.com>
2025-07-17 21:38:48 +0900

    feat: :sparkles: 画面サイズによってAmttの文字サイズを適切に変更

--- src/layouts/v2/Home.tsx
+++ src/layouts/v2/Home.tsx
@@ -6,17 +6,17 @@ import Logo from "@/components/icons/logo.svg";
 export default function Home() {
 	return (
 		<div className="bg-gray-100 min-h-screen min-w-screen p-8">
-			<div className="w-3/4 lg:w-1/4 mx-auto">
+			<div className="w-3/4 md:w-1/2 lg:w-1/4 mx-auto">
 				<div className="w-full pt-12 pr-12 pl-12">
 					<div className="rounded-[28px] bg-white p-[8px]">
 						<Logo className="rounded-[20px]" />
 					</div>
+					<p className="text-[140%] tracking-widest my-6">Amtt</p>
 				</div>
 
-				<div className="px-7">
-					<p className="text-[30px] tracking-widest my-6">Amtt</p>
+				<div className="pr-12 pl-12">
 					<div className="flex space-x-9">
-						<div className="h-[154px] w-[1px] bg-blue-400 mb-7" />
+						<div className="h-[10vw] w-[1px] bg-blue-400 mb-7" />
 						<div className="space-y-5 text-sm tracking-widest">
 							<div className="flex space-x-5 items-center">
 								<Taiyaki className="w-[24px]" />
