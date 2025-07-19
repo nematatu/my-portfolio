af8863c76c548aab2308afb031ba9c5fdb46c5b0
nematatu <nematatu1003@gmail.com>
2025-07-19 13:06:50 +0900

    style: :lipstick: 大きさ調整

--- src/layouts/v2/Home.tsx
+++ src/layouts/v2/Home.tsx
@@ -6,7 +6,7 @@ import Logo from "@/components/icons/logo.svg";
 export default function Home() {
 	return (
 		<div className="bg-gray-100 min-h-screen min-w-screen p-8">
-			<div className="w-full md:w-1/2 lg:w-1/3 mx-auto">
+			<div className="w-full md:w-1/2 lg:w-1/4 mx-auto">
 				<div className="w-full pt-12 pr-12 pl-12 mb-7">
 					<div className="rounded-[28px] p-[8px] border-[1px] bg-white border-solid border-blue-400 ">
 						<Logo className="rounded-[20px]" />
