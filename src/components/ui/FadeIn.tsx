bb3635dd696d2b5a16167ed7c4adeba42ae5ff68
nematatu <nematatu1003@gmail.com>
2025-07-24 04:31:15 +0900

    style: :lipstick: durationを上げた

--- src/components/ui/FadeIn.tsx
+++ src/components/ui/FadeIn.tsx
@@ -17,7 +17,7 @@ export function FadeIn(props: ComponentPropsWithoutRef<typeof motion.div>) {
 	return (
 		<motion.div
 			transition={{
-				duration: 0.6,
+				duration: 0.8,
 				ease: [0.4, 0, 0.2, 1],
 			}}
 			variants={{
