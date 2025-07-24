b2e82783d7c0e44e42215559cf98ec01c8102e98
nematatu <nematatu1003@gmail.com>
2025-07-25 02:01:32 +0900

    feat: :sparkles: twitter ogp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,31 +4,42 @@ import { zen_Kaku_Gothic_New } from "@/utils/fonts/v1/fonts";
 import { Providers } from "./providers";
 
 export const metadata: Metadata = {
-	metadataBase: new URL("https://amatatu.com"),
-	openGraph: {
-    type: "website",
-		title: "amatatu",
-		description: "amatatu's portfolio",
-    url: "https://amatatu.com",
-    images: {
-            url: "/assets/icon.png", 
-            type: "image/png", 
-        }, 
-	},
+    metadataBase: new URL("https://amatatu.com"),
+    openGraph: {
+        type: "website",
+        title: "amatatu",
+        description: "amatatu's portfolio",
+        url: "https://amatatu.com",
+        images: {
+            url: "/assets/icon.png",
+            type: "image/png",
+        },
+    },
+    twitter: {
+        type: "website",
+        title: "amatatu",
+        description: "amatatu's portfolio",
+        url: "https://amatatu.com",
+        images: {
+            url: "/assets/icon.png",
+            type: "image/png",
+        },
+        card: "summary",
+    },
 };
 
 export default function RootLayout({
-	children,
+    children,
 }: Readonly<{
-	children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-	return (
-		<html lang="ja">
-			<body className={`${zen_Kaku_Gothic_New.className} antialiased`}>
-				<div className="relative bg-gray-100 min-h-screen min-w-screen p-8 text-gray-700 dark:text-gray-200 dark:bg-gray-700 tracking-widest">
-					<Providers>{children}</Providers>
-				</div>
-			</body>
-		</html>
-	);
+    return (
+        <html lang="ja">
+            <body className={`${zen_Kaku_Gothic_New.className} antialiased`}>
+                <div className="relative bg-gray-100 min-h-screen min-w-screen p-8 text-gray-700 dark:text-gray-200 dark:bg-gray-700 tracking-widest">
+                    <Providers>{children}</Providers>
+                </div>
+            </body>
+        </html>
+    );
 }
