00d0cc6a9501a088612614c167c550dad83cf9cd
nematatu <nematatu1003@gmail.com>
2025-07-25 15:38:04 +0900

    feat: :sparkles: はてなブログアイコン追加した

---
+++ src/components/ui/HatenaIcon.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+import HatenaIcon from "@/components/icons/hatenablog-logo.svg";
+import HatenaIconDark from "@/components/icons/hatenablog-logo-dark.svg";
+
+export default function HatenaIconSwitch() {
+	const [mounted, setMounted] = useState(false);
+	const { setTheme, resolvedTheme } = useTheme();
+
+	useEffect(() => setMounted(true), []);
+
+	if (!mounted) {
+		return null;
+	}
+
+	if (resolvedTheme === "dark") {
+		return (
+			<div className="w-8 h-8">
+				<HatenaIconDark />
+			</div>
+		);
+	}
+
+	if (resolvedTheme === "light") {
+		return (
+			<div className="w-8 h-8">
+				<HatenaIcon />
+			</div>
+		);
+	}
+}
--- src/layouts/v2/Home.tsx
+++ src/layouts/v2/Home.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import Logo from "@/components/icons/logo.svg";
 import { EaseDriver, FadeIn, FadeInWithStagger } from "@/components/ui/FadeIn";
+import HatenaIconSwitch from "@/components/ui/hatenaIcon";
 import ThemeSwitch from "@/components/ui/ThemeSwitch";
 
 export default function Home() {
@@ -61,13 +62,17 @@ export default function Home() {
 						</FadeIn>
 						<FadeIn>
 							<Link
-								href="/blog"
-								className=" transition-colors hover:text-blue-500"
+								target="_blank"
+								href="https://amtt.hatenablog.com/"
+								className="transition-colors hover:text-blue-500"
 							>
-								<div className="p-3 flex space-x-8 items-center">
+								<div className="p-3 flex space-x-4 items-center">
 									<div className="w-[11px] h-[11px] md:w-[13px] md:h-[12px] rounded-[3px] bg-white dark:bg-gray-800 border-gray-700 dark:border-gray-200 border-solid border-[1.5px]" />
-									<p className="font-bold">Blog</p>
-									<p className="absolute left-24">{">"}</p>
+									<div className=" flex items-center space-x-1">
+										<HatenaIconSwitch />
+										<p className="font-bold">Blog</p>
+									</div>
+									<p className="absolute left-28">{">"}</p>
 								</div>
 							</Link>
 						</FadeIn>
